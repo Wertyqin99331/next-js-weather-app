fcbb4e8e38cf04750243b4dac2ce0126452e905a
Wertyqin99331 <flywotbl@mail.ru>
2023-11-21 20:02:42 +0500

    kek

--- src/components/WeatherWidget.tsx
+++ src/components/WeatherWidget.tsx
@@ -1,6 +1,7 @@
 import { IGetWeatherDTO } from '@/types/IGetWeatherDTO';
 import { WeatherInfo } from '@/components/WeatherInfo';
 import { YandexMap } from '@/components/YandexMap';
+import { WEATHER_API_URL } from '@/const/api';
 
 type WeatherWidgetProps = {
   lat: number,
@@ -8,7 +9,12 @@ type WeatherWidgetProps = {
 }
 
 const WeatherWidget = async ({ lat, lon }: WeatherWidgetProps) => {
-  const res = await fetch(`/api/weather?lat=${ lat }&lon=${ lon }`).then(res => res.json()) as IGetWeatherDTO
+  const res = await fetch(`${ WEATHER_API_URL }?q=${ lat },${ lon }&key=${ process.env.WEATHER_API_KEY }`, {
+    method: 'GET',
+    next: {
+      revalidate: 60
+    }
+  }).then(res => res.json() as Promise<IGetWeatherDTO>)
 
   return (
     <div className='pt-40 text-black text-3xl font-normal flex flex-col gap-y-4 items-center'>
